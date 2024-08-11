3d791a0bda5d43cef697a121e01b2806158dd158
Ehsann123 <ehsanssb@gmail.com>
2024-08-11 22:19:27 +0500

    pythonCode

---
+++ Face-Unlock-main/README.md
@@ -0,0 +1,14 @@
+# Face-Unlock
+A Face Unlocker Project using OpenCV-Python
+<br><br>
+
+### The code first of all collects 250 images of a user and saves the user as the password.
+<br>
+
+<b> Note: </b>Before executing the code, please create a folder with the name "dataset" in the same directory where "data_collection.py" is situated, else, it will throw an error
+<br>
+
+### In the next step when a face is exposed to the trained model, it detects that if the face is of correct user or not....
+
+<br>
+Regards
---
+++ Face-Unlock-main/train_model.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile,join
+
+data_path = 'dataset/'
+only_files = [f for f in listdir(data_path) if isfile(join(data_path,f))]
+
+Training_Data, Labels = [], []
+
+for i, files in enumerate(only_files):
+    image_path = data_path + only_files[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
+
+Labels = np.asarray(Labels, dtype=np.int32)
+
+model = cv2.face.LBPHFaceRecognizer_create()
+
+model.train(np.asarray(Training_Data), np.asarray(Labels))
+
+print('Model Training Completed')
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def face_detector(img, size = 0.5):
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_classifier.detectMultiScale(gray,1.3,5)
+	if len(faces) == 0:
+		return img,[]
+		
+	for(x,y,w,h) in faces:
+		cv2.rectangle(img,(x,y),(x+w, y+h),(0,255,255),2)
+		roi = img[y:y+h, x:x+w]
+		roi = cv2.resize(roi,(200,200))
+		
+		return img,roi
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = cap.read()
+	
+	image,face = face_detector(frame)
+	
+	try:
+		face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+		result = model.predict(face)
+		
+		if result[1] < 500:
+			confidence = int((1-(result[1])/300)*100)
+			display_string = str(confidence)+'% Chances of being a correct user'
+		cv2.putText(image,display_string,(50,120),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,255),4)
+		
+		if confidence>85:
+			cv2.putText(image,"Unlocked",(250,450),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),4)
+			cv2.imshow('Face Scanner', image)
+		else:
+			cv2.putText(image,"Locked",(250,450),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),4)
+			cv2.imshow('Face Scanner', image)
+		
+	except:
+		cv2.putText(image,"Face Not Found",(250,450),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255), 4)		
+		cv2.imshow('Face Scanner', image)
+		pass
+		
+	if cv2.waitKey(1) == 13:
+		break
+		
+cap.release()
+cv2.destroyAllWindows()
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
